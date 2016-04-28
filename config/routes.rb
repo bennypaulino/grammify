5906aad39a09b7e78ecba660b73c6f82295a77c2
bennypaulino <santiagopro71@gmail.com>
2016-04-28 14:59:52 -0400

    Build tests for adding a detail page for grams, ensure that the tests pass and add the show page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Grammify::Application.routes.draw do
   devise_for :users, :controllers=>{ registrations:'registrations' }
   root "grams#index"
-  resources :grams, only: [:new, :create]
+  resources :grams, only: [:new, :create, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
